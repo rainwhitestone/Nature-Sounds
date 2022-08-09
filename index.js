57f9085c39ea3a3d76a127f4566d9d89bfbcbd7c
Yağmur AKTAŞ <72444387+rainwhitestone@users.noreply.github.com>
2022-08-09 17:28:05 +0300

    update 3

--- index.js
+++ index.js
@@ -36,41 +36,41 @@ function handleClick (){
 
     switch(key) {
         case "n":
-            var audio = new Audio("/nature/sounds/bird.wav");
+            var audio = new Audio("/sounds/bird.wav");
             audio.play();
     
         break;
     
     
         case "a":
-            var audio = new Audio("/nature/sounds/bird-2.wav");
+            var audio = new Audio("/sounds/bird-2.wav");
             audio.play();
         
         break;
     
     
         case "t":
-            var audio = new Audio("/nature/sounds/bird-3.wav");
+            var audio = new Audio("/sounds/bird-3.wav");
             audio.play();
             
         break;
     
     
         case "u":
-            var audio = new Audio("/nature/sounds/night.wav");
+            var audio = new Audio("/sounds/night.wav");
             audio.play();
                 
         break;
         
         case "r":
-            var audio = new Audio("/nature/sounds/storm.wav");
+            var audio = new Audio("/sounds/storm.wav");
             audio.play();
                 
         break;
     
         
         case "e":
-            var audio = new Audio("/nature/sounds/storm-2.wav");
+            var audio = new Audio("/sounds/storm-2.wav");
             audio.play();
                 
         break;
